884238dcefb233958100207f8af7737482b2eff0
Kryzhanivskyi89 <andrew.506@ukr.net>
2023-01-21 13:26:19 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,7 +7,7 @@ const refs = {
 };
 refs.sudmitBtn.addEventListener('click', (event) => {
   event.preventDefault();  
-  for (let i = 1; i <= 4; i += 1) {
+  for (let i = 1; i <= refs.amount; i += 1) {
     let position = i;
     let delay = refs.firstDelay;       
     console.log(delay)
