7cfa4250e8ead5be74a37826ddaaf6bbefda418a
Kryzhanivskyi89 <andrew.506@ukr.net>
2023-01-22 11:30:37 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,48 +7,29 @@ const refs = {
 const changeColor = {
   intervalId: null,
   isActive: false,
-           
   start() {
-    this.isActive = true;
-    this.intervalId = setInterval(() => {
+    isActive = true;
+    intervalId = setInterval(() => {
       refs.body.style.backgroundColor = getRandomHexColor();
     }, 1000)
   },
-
   stop() {
-    clearInterval(this.intervalId); 
-    this.isActive = false;   
-  }
+    clearInterval(intervalId);
+    isActive = false;
+  },
+};
 
-}
-  
 refs.startButton.addEventListener('click', () => {  
   changeColor.start();  
+  refs.startButton.setAttribute('disabled', '');
 });
 
- refs.stopButton.addEventListener('click', () => {  
-  changeColor.stop();  
+refs.stopButton.addEventListener('click', () => {  
+   changeColor.stop();  
+   refs.startButton.removeAttribute('disabled', '');
 });
 
 function getRandomHexColor() {  
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-
-// refs.startButton.addEventListener('click', changeColor);
-
-// function getRandomHexColor() {  
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// let intervalId = null;
-// function changeColor() {
-  
-//   intervalId = setInterval(() => {
-//       refs.body.style.backgroundColor = getRandomHexColor(); 
-//     }, 1000)
-// }
-      
-// refs.stopButton.addEventListener('click', () => {  
-//   clearInterval(intervalId);  
-// });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,25 +3,23 @@ const refs = {
   firstDelay: form.firstChild.parentNode[0],
   delayStep: form.firstChild.parentNode[1],
   amount: form.firstChild.parentNode[2],  
-  sudmitBtn: document.querySelector("button")
+  sudmitBtn: document.querySelector("button")   
 };
 refs.sudmitBtn.addEventListener('click', (event) => {
-  event.preventDefault();  
+  event.preventDefault();
+  let delay = refs.firstDelay.value; 
   for (let i = 1; i <= refs.amount.value; i += 1) {
     let position = i;
-    let delay = refs.firstDelay.value;       
-    console.log(delay)
-    console.log(position)
-    createPromise(position, delay)   
+    createPromise(position, delay)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });    
-      delay += refs.delayStep.value;
-  };    
-})
+      });
+    delay = Number(delay) + Number(refs.delayStep.value);
+  };
+});
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
@@ -32,6 +30,5 @@ function createPromise(position, delay) {
         reject ({ position, delay });
       };
     }, delay);
-  });
-     
+  });     
 };
